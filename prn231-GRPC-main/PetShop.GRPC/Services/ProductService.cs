2741baaa8dbdbd126cef2a4cfe99fcede180e014
vinnt060402 <vinnt060402@gmail.com>
2024-03-18 22:58:45 +0700

    Assignment3

---
+++ prn231-GRPC-main/PetShop.GRPC/Services/ProductService.cs
@@ -0,0 +1,108 @@
+﻿using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using PetShop.BusinessObject.Models;
+using PetShop.Repositories.Implements;
+using PetShop.Repositories.Interfaces;
+using PetShop.Services.Enums;
+
+namespace PetShop.GRPC.Services
+{
+    public class ProductService : ProductServices.ProductServicesBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public override Task<ProductResponse> CreateProduct(CreateProductRequest request, ServerCallContext context)
+        {
+            if(request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var productItems = new Product{
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Price = request.Price,
+                Status = request.Status,               
+            };
+            _productRepository.Create(productItems);
+            return Task.FromResult(new ProductResponse
+            {
+                Id = productItems.Id.ToString(),
+                Name = productItems.Name,
+                Price = (int)productItems.Price,
+                Status = productItems.Status,
+            });
+        }
+
+        public override Task<GetAllProductsResponse> GetAllProducts(GetAllProductsRequest request, ServerCallContext context)
+        {
+            if(request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var response = new GetAllProductsResponse();
+            var products = _productRepository.Get().ToList();
+            foreach (var product in products)
+            {
+                response.Product.Add(new ProductResponse 
+                {
+                    Id = product.Id.ToString(),
+                    Name = product.Name,
+                    Price= (int)product.Price,
+                    Status = product.Status,
+                    CategoryId = product.CategoryId.ToString()
+                });
+            }
+            return Task.FromResult(response);
+        }
+
+        public override Task<ProductResponse> UpdateProduct(UpdateProductRequest request, ServerCallContext context)
+        {
+            if(request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            Guid? categoryId = null;
+            if (!string.IsNullOrEmpty(request.CategoryId) && Guid.TryParse(request.CategoryId, out Guid parsedId))
+            {
+                categoryId = parsedId;
+            }
+            var product = _productRepository.Get(request.Id);
+            if (product == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Cannot find this product or product does not exist"));
+            } 
+            else
+            {
+                product.Name = request.Name;
+                product.Price = request.Price;
+                product.Status = request.Status;
+                product.CategoryId = categoryId;
+            }
+            _productRepository.Update(product);
+            return Task.FromResult(new ProductResponse
+            {
+                Id = product.Id.ToString()
+            });
+        }
+
+        public override Task<ProductResponse> DeleteProduct(DeleteProductRequest request, ServerCallContext context)
+        {
+            if(request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var product = _productRepository.Get(request.Id);
+            if(product == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Cannot find this product or product does not exist"));
+            }
+            _productRepository.Delete(product);
+            return base.DeleteProduct(request, context);
+        }
+    }
+}
---
+++ prn231-GRPC-main/PetShop.GRPC/Services/UserService.cs
@@ -0,0 +1,113 @@
+﻿using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using PetShop.BusinessObject.Models;
+using PetShop.Repositories.Implements;
+using PetShop.Repositories.Interfaces;
+using PetShop.Services.Enums;
+
+namespace PetShop.GRPC.Services
+{
+    public class UserService : UserServices.UserServicesBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public override Task<UserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
+        {
+            if (request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var userItems = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = request.Username,
+                Email = request.Email,
+                Password = request.Password,
+                Role = request.Role,    
+                Status = request.Status,
+            };
+            _userRepository.Create(userItems);
+            return Task.FromResult(new UserResponse
+            {
+                Id = userItems.Id.ToString(),
+                Status = userItems.Status,
+                Role = userItems.Role,
+                Username = userItems.Username,
+                Email = userItems.Email,
+                Password = userItems.Password,
+            });
+        }
+
+        public override Task<GetAllUserReponse> GetAllUsers(GetAllUserRequest request, ServerCallContext context)
+        {
+            if (request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var response = new GetAllUserReponse();
+            var users = _userRepository.Get().Where(x => x.Status == UserStatus.Activate.ToString());
+            foreach (var user in users)
+            {
+                response.User.Add(new UserResponse
+                {
+                    Id = user.Id.ToString(),
+                    Status = user.Status,
+                    Role = user.Role,
+                    Username = user.Username,
+                    Email = user.Email,
+                    Password = user.Password,
+                });
+            }
+            return Task.FromResult(response);
+        }
+
+        public override Task<UserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+        {
+            if (request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var user = _userRepository.Get(request.Id);
+            if (user == null)
+            {
+                throw new Exception("Cannot find this user");
+            }
+            else
+            {
+                user.Username = request.Username;
+                user.Email = request.Email;
+                user.Password = request.Password;
+                user.Status = request.Status;
+                user.Role = request.Role;
+            }
+            _userRepository.Update(user);
+            return Task.FromResult(new UserResponse
+            {
+                Id = user.Id.ToString()
+            });
+        }
+
+        public override Task<UserResponse> DeleteUser(DeleteUserRequest request, ServerCallContext context)
+        {
+            if (request == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error request"));
+            }
+            var user = _userRepository.Get(request.Id);
+            if (user == null)
+            {
+                throw new Exception("Cannot find this user");
+            }
+            else
+            {
+                _userRepository.Delete(user);
+            }
+            return base.DeleteUser(request, context);
+        }
+    }
+}
